afe24d30d50ac1ffda46fd79e77c940a3698cd89
Amitabh1998 <amitabh.das1998@gmail.com>
2024-07-31 17:20:09 -0400

    modifications

--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from "react";
 import * as client from "./client";
-import { FaPlus } from "react-icons/fa";
 export default function PeopleTable() {
   const [users, setUsers] = useState<any[]>([]);
   const [role, setRole] = useState("");
+  const filterUsersByRole = async (role: string) => {
+    setRole(role);
+    if (role) {
+      const users = await client.findUsersByRole(role);
+      setUsers(users);
+    } else {
+      fetchUsers();
+    }
+  };
   const [name, setName] = useState("");
   const filterUsersByName = async (name: string) => {
     setName(name);
@@ -15,30 +23,7 @@ export default function PeopleTable() {
     }
   };
 
-  const filterUsersByRole = async (role: string) => {
-    setRole(role);
-    if (role) {
-      const users = await client.findUsersByRole(role);
-      setUsers(users);
-    } else {
-      fetchUsers();
-    }
-  };
 
-  const createUser = async () => {
-    const user = await client.createUser({
-      firstName: "New",
-      lastName: `User${users.length + 1}`,
-      username: `newuser${Date.now()}`,
-      password: "password123",
-      section: "S101",
-      role: "STUDENT",
-    });
-    setUsers([...users, user]);
-  };
-
-
-  
   const fetchUsers = async () => {
     const users = await client.findAllUsers();
     setUsers(users);
@@ -48,13 +33,9 @@ export default function PeopleTable() {
   }, []);
   return (
     <div id="wd-people-table">
-        <button onClick={createUser} className="float-end btn btn-danger wd-add-people">
-        <FaPlus className="me-2" />
-        People
-      </button>
-        <input onChange={(e) => filterUsersByName(e.target.value)} placeholder="Search people"
+      <input onChange={(e) => filterUsersByName(e.target.value)} placeholder="Search people"
              className="form-control float-start w-25 me-2 wd-filter-by-name" />
-        <select value={role} onChange={(e) =>filterUsersByRole(e.target.value)}
+      <select value={role} onChange={(e) =>filterUsersByRole(e.target.value)}
               className="form-select float-start w-25 wd-select-role" >
         <option value="">All Roles</option>        <option value="STUDENT">Students</option>
         <option value="TA">Assistants</option>     <option value="FACULTY">Faculty</option>
@@ -84,4 +65,3 @@ export default function PeopleTable() {
     </div>
   );
 }
-
